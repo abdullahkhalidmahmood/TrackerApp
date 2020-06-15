d128d7d60b008f230f2780e5525445db12851ed1
abdullahkhalidmahmood <100064352@students.swinburne.edu.my>
2020-06-15 15:28:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/trackerapp/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.trackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.audiofx.Equalizer;
+import android.os.Bundle;
+import android.os.LocaleList;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.w3c.dom.Text;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION = 1;
+    Button getlocationBtn;
+    TextView showLocationTxt;
+
+    LocationManager locationManager;
+    String latitude, longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Adding permission
+        ActivityCompat.requestPermissions(this, new String[]
+                {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        showLocationTxt = findViewById(R.id.result);
+        getlocationBtn = findViewById(R.id.buttonStart);
+
+        getlocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                //check if gps is anable or not
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    //enable gps here
+
+                    OnGPS();
+                } else {
+                    // GPS is ON
+                    getLocation();
+                }
+            }
+        });
+    }
+
+    private void getLocation() {
+        //Check permission again
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location LocationGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location LocationNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location LocationPassive = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (LocationGps != null) {
+                double lat = LocationGps.getLatitude();
+                double longi = LocationGps.getLongitude();
+
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+
+                showLocationTxt.setText("Your Location:" + "\n" + "Latitude= " +
+                        latitude + "\n" + "Longitude= " + longitude);
+
+            } else if (LocationNetwork != null) {
+                double lat = LocationNetwork.getLatitude();
+                double longi = LocationNetwork.getLongitude();
+
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+
+                showLocationTxt.setText("Your Location:" + "\n" + "Latitude= " +
+                        latitude + "\n" + "Longitude= " + longitude);
+
+            } else if (LocationPassive != null) {
+                double lat = LocationPassive.getLatitude();
+                double longi = LocationPassive.getLongitude();
+
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+
+                showLocationTxt.setText("Your Location:" + "\n" + "Latitude= " +
+                        latitude + "\n" + "Longitude= " + longitude);
+
+            } else {
+                Toast.makeText(this, "Unable to get your location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    //HTTP connection to the URL
+    //
+
+}
